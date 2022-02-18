329edaa5f3488a9631464ded3a8d0cb8328f9d70
Ivan Suprun <bender.slam@gmail.com>
2022-02-18 16:50:54 +0200

    contacts & index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Command } = require("commander");
+const contactsMethods = require("./contacts.js");
+
+const program = new Command();
+
+program
+  .addOption(new Option("-a, --action <type>", "choose action").choices(["list", "get", "remove", "add"]))
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contactsMethods.listContacts();
+      break;
+
+    case "get":
+      contactsMethods.getContactById(id);
+      break;
+
+    case "add":
+      contactsMethods.addContact(name, email, phone);
+      break;
+
+    case "remove":
+      contactsMethods.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
